92c47132d75149ceb1064eb2f8aa994fcd0c15d4
Diegosou4 <diegoaguia31@gmail.com>
2023-11-06 10:54:57 +0000

    Done

--- get_next_line.c
+++ get_next_line.c
@@ -3,14 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: diemorei <diemorei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:52:58 by diegmore          #+#    #+#             */
-/*   Updated: 2023/11/02 16:53:00 by diegmore         ###   ########.fr       */
+/*   Updated: 2023/11/06 10:43:28 by diemorei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	size_t	size;
+	size_t	i;
+
+	i = 0;
+	size = ft_strlen((char *)src);
+	dest = (char *)calloc((size + 1), sizeof(char));
+	if (!dest)
+		return (NULL);
+	while (i < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
 
 char    *get_next_line(int fd)
 {
@@ -23,7 +53,7 @@ char    *get_next_line(int fd)
         return (NULL);
     str = malloc(sizeof(char) * BUFFER_SIZE);
     j = read(fd, str, BUFFER_SIZE);
-    while(str[0], str[i] != '\n')
+    while(str[0] && str[i] != '\n')
     {
         if(str[i] == '\0')
             break;
@@ -42,14 +72,13 @@ char    *get_next_line(int fd)
 }
 
 
-int main(int argc, char *argv[])
+int main()
 {
-    if(argc == 2)
-    {
-        int fd = open(argv[1], O_RDONLY);
+    
+        int fd = open("string.txt", O_RDONLY);
         char *str = get_next_line(fd);
         printf("%s", str);
         printf("%s", get_next_line(fd));
-    }
+
 
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: diemorei <diemorei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:53:23 by diegmore          #+#    #+#             */
-/*   Updated: 2023/11/02 16:53:24 by diegmore         ###   ########.fr       */
+/*   Updated: 2023/11/06 10:43:32 by diemorei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,3 @@
 
 
 
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-char	*ft_strdup(char *src)
-{
-	char	*dest;
-	size_t	size;
-	size_t	i;
-
-	i = 0;
-	size = ft_strlen((char *)src);
-	dest = (char *)calloc((size + 1), sizeof(char));
-	if (!dest)
-		return (NULL);
-	while (i < size)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	return (dest);
-}
